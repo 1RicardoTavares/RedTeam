01a9a03c251689c5c5b139cc6ef2ef9dd7bd6893
1RicardoTavares <ricardo@tavares.io>
2020-09-16 21:38:19 -0300

    Update install.sh
    Update CrackMapExec install

--- C2/install.sh
+++ C2/install.sh
@@ -107,7 +107,8 @@ git clone https://github.com/lgandx/Responder.git
 # CrackMapExec - Python 3
 cd /tmp/ 
 git clone --recursive https://github.com/byt3bl33d3r/CrackMapExec && cd CrackMapExec
-sudo python3 setup.py install
+sudo pip install -r requirements.txt
+sudo python setup.py install
 cd ..
 sudo rm -rf CrackMapExec
 
